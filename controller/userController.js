2758f71eee64d00ec41f0c44e9566ed7c6aaaecb
samyukth vinod <vinodsamyukth@gmail.com>
2023-08-02 16:03:38 +0530

    added limit in home page

--- controller/userController.js
+++ controller/userController.js
@@ -447,7 +447,7 @@ const personalChoice = async (req, res) => {
     const personal = await Event.find({
       eventOrganizer: { $in: organizers },
       
-    });
+    }).limit(4);
 
 
     res.json({ personal });
